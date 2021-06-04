e4c785bd47fffecb82a7d12ad8c4647778e0e7f0
jdburgos <j.dburgos@hotmail.com>
2021-06-04 10:26:15 -0500

    feat: add environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyD7k0vhjXK-RFrsUbL95fdhqSYzBOVTqAo",
-  authDomain: "react-todo-app-92619.firebaseapp.com",
-  databaseURL: "https://react-todo-app-92619-default-rtdb.firebaseio.com",
-  projectId: "react-todo-app-92619",
-  storageBucket: "react-todo-app-92619.appspot.com",
-  messagingSenderId: "448380857067",
-  appId: "1:448380857067:web:cf2611fca72481838b6cae"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABSE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config);
