b9189493627ac6f0a5f12ec55b6c431b5ed67de3
Juan david burgos oquendo <j.dburgos@hotmail.com>
2021-07-25 16:39:43 -0500

    feat: delete TodoFilters

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
 import TodoForm from './components/TodoForm/TodoForm';
 import Container from './components/layout/Container/Container';
-import TodoFilter from './components/TodoFilter/TodoFilter';
 
 const App = () => (
   <Wrapper>
@@ -18,7 +17,6 @@ const App = () => (
     <Container>
       <TodoForm />
       <TodoList />
-      <TodoFilter />
     </Container>
   </Wrapper>
 );
--- src/components/TodoFilter/TodoFilter.js
+++
@@ -1,9 +0,0 @@
-/** React core **/
-import React from 'react';
-
-/** Components **/
-import Card from '../ui/Card/Card';
-
-const TodoFilter = () => <Card>Filters</Card>;
-
-export default TodoFilter;
